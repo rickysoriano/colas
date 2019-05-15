5554eb4fe558d393bafe13b6ee63070a3c50f8c1
ricky_s01 <ricky_s01@hotmail.es>
2019-05-15 13:35:24 -0500

    crear vector

--- colas/src/vistas/Colas.java
+++ colas/src/vistas/Colas.java
@@ -7,5 +7,10 @@ package vistas;
 public class Colas {
     public static void main(String [] args) {
         System.out.print("Hola");
+        
+        String vAlumnos [] = new String [3];
+        
+        vAlumnos [0] = "Martin";
+        vAlumnos [1] = "JOse";
     }
 }
